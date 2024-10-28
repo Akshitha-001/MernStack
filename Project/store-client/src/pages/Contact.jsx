9965c003926a32185a03552dfe1de947a9148147
Akshitha-001 <aligetiakshitha@gmail.com>
2024-10-28 18:45:57 +0530

    store-client

---
+++ Project/store-client/src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <div className='w-screen h-full flex flex-col justify-center items-center'>
+        <div className='w-full h-40 flex justify-center items-center text-3xl text-purple-500 font-semibold'>
+               Contact Us
+        </div>
+    
+    </div>
+    
+     
+    
+    </>
+    
+  )
+}
+
+export default Contact
