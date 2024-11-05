f3c1b85aef66e842689fcb3d699d7d91f916a231
Akshitha-001 <aligetiakshitha@gmail.com>
2024-11-05 14:35:12 +0530

    Dashboard created

--- Project/store-client/src/pages/Admin/AdminSettings.jsx
+++ Project/store-client/src/pages/Admin/AdminSettings.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const AdminSettings = () => {
   return (
-    <div>
-      
-    </div>
+    <div>AdminSettings</div>
   )
 }
 
--- Project/store-client/src/pages/Admin/AdminUsers.jsx
+++ Project/store-client/src/pages/Admin/AdminUsers.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const AdminUsers = () => {
   return (
-    <div>
-      
-    </div>
+    <div>AdminUsers</div>
   )
 }
 
